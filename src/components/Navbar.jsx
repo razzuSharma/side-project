a8379bbaa21110736a6e34216e148bf61036ddc3
razzuSharma <razzusharma008@gmail.com>
2023-04-05 12:09:33 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-items">
+        <li>Home</li>
+        <li>Products</li>
+        <li>About Us</li>
+        <li>Profile</li>
+
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  // ...
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
